b10fe4c1f09fe7646d2c8792d6332658c1bf4704
Victor Helppiquet <victor.helpiquet@domaine>
2026-02-10 20:14:14 +0100

    fonction print des données

--- vol.py
+++ vol.py
@@ -15,94 +15,44 @@ print(spy.head())
 
 # Data treatment
 
+# Printing data function
+def print_data(x,y,mode,name,title,x_title,y_title):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+    x=x,
+    y=y,
+    mode=mode,
+    name=name
+    ))
+    fig.update_layout(
+        title=title,
+        xaxis_title=x_title,
+        yaxis_title=y_title,
+        hovermode="x unified"
+    )
+    fig.show()
+    return(0)
+    
+# Volume data
 spy_vol = spy["Volume"].squeeze()
 print(spy_vol.head()) 
 
-# Print data
+print_data(spy_vol.index,spy_vol.values,'lines+markers','Volume',"SPY Intraday Volume (30-min)","Datetime","Volume")
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(
-    x=spy_vol.index,
-    y=spy_vol.values,
-    mode='lines+markers',
-    name='Volume'
-))
-fig.update_layout(
-    title="SPY Intraday Volume (30-min)",
-    xaxis_title="Datetime",
-    yaxis_title="Volume",
-    hovermode="x unified"
-)
-fig.show()
-
-# ACF and PACF
-
+#ACF and PACF
 # Compute ACF and PACF
 lags = 60
 acf_vals = acf(spy_vol, nlags=lags)
 pacf_vals = pacf(spy_vol, nlags=lags)
 
-# Plot ACF and PACF
+print_data(list(range(lags + 1)),acf_vals,'lines+markers','ACF',"ACF of SPY Intraday Volume","Lag","ACF")
+print_data(list(range(lags + 1)),pacf_vals,'lines+markers','PACF',"PACF of SPY Intraday Volume","Lag","PACF")
 
-fig_acf = go.Figure()
-fig_acf.add_trace(go.Scatter(
-    x=list(range(lags + 1)),
-    y=acf_vals,
-    mode='lines+markers',
-    name='ACF'
-))
-fig_acf.update_layout(
-    title="ACF of SPY Intraday Volume",
-    xaxis_title="Lag",
-    yaxis_title="ACF",
-    hovermode="x unified"
-)
-fig_acf.show()
-
-fig_pacf = go.Figure()
-fig_pacf.add_trace(go.Scatter(
-    x=list(range(lags + 1)),
-    y=pacf_vals,
-    mode='lines+markers',
-    name='PACF'
-))
-fig_pacf.update_layout(
-    title="PACF of SPY Intraday Volume",
-    xaxis_title="Lag",
-    yaxis_title="PACF",
-    hovermode="x unified"
-)
-fig_pacf.show()
-
+# moyenne mobile
 vol_lisse = spy_vol.rolling(window=126).mean()
+print_data(spy_vol.index,vol_lisse.values,'lines','Smoothed Volume',"Smoothed SPY Intraday Volume (30-day rolling mean)","Datetime","Smoothed Volume")
 
-fig_lisse = go.Figure()
-fig_lisse.add_trace(go.Scatter(
-    x=spy_vol.index,
-    y=vol_lisse.values,
-    mode='lines',
-    name='Smoothed Volume'
-))
-fig_lisse.update_layout(
-    title="Smoothed SPY Intraday Volume (30-day rolling mean)",
-    xaxis_title="Datetime",
-    yaxis_title="Smoothed Volume",
-    hovermode="x unified"
-)
-fig_lisse.show()
+# différentiation
 
 vol_diff = spy_vol.diff()
-fig_diff = go.Figure()
-fig_diff.add_trace(go.Scatter(
-    x=vol_diff.index,
-    y=vol_diff.values,
-    mode='lines',
-    name='Differenced Volume'
-))
-fig_diff.update_layout(
-    title="Differenced SPY Intraday Volume",
-    xaxis_title="Datetime",
-    yaxis_title="Differenced Volume",
-    hovermode="x unified"
-)
-fig_diff.show()
+print_data(vol_diff.index,vol_diff.values,'lines','Differenced Volume',"Differenced SPY Intraday Volume","Datetime","Differenced Volume")
